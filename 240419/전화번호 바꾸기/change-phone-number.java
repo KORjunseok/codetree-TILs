d675192ad65551646186b6e6db8795663611de8b
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-04-19 16:30:05 +0900

    Title: 전화번호 바꾸기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240419/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        int num = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        System.out.print("010-" + y + "-" + x);
+    }
+}
