30c0aad827916b697377f2f22987f030f41f6e74
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-04-24 16:08:02 +0900

    Title: 점수 비교, Time: 113ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240424/점수 비교/score-comparison.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int b2 = sc.nextInt();
+
+        if (a1 > b1 && b1 > b2) System.out.print(1);
+        else System.out.print(0);
+
+    }
+}
