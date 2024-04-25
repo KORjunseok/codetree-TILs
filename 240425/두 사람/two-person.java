b2b1c836f85e3d31553dda7506483b6b5ccaa4d5
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-04-25 17:15:46 +0900

    Title: 두 사람, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240425/두 사람/two-person.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char as = sc.next().charAt(0);
+
+        int b = sc.nextInt();
+        char bs = sc.next().charAt(0);
+
+        if ((a >= 19 && b >= 19) && ((as == 'M') || (bs == 'M'))) System.out.print(1);
+        else System.out.print(0);
+    }
+}
