34d12474149651faf8db009627edb5a7f7bc8c09
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-05-03 13:29:11 +0900

    Title: 20대의 평균나이, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240503/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        int sum = 0;
+
+        while (true) {
+            int n = sc.nextInt();
+            if (n / 10 != 2 ) {
+                System.out.printf("%.2f",(double) sum / cnt);
+                break;
+                
+            }
+
+            sum += n;
+            cnt++;
+            
+
+        }
+
+    }
+}
