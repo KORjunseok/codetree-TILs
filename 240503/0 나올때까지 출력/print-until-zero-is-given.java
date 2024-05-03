50852994eee3d57952ac661e41558555d78c7f29
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-05-03 13:03:55 +0900

    Title: 0 나올때까지 출력, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240503/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i <= 100; i++) {
+            int n = sc.nextInt();
+            
+            if (n == 0)
+            break;
+            System.out.println(n);
+
+
+                
+            }
+        }
+
+    }
