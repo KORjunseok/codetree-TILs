41d996de9d253f80eb2553ecbadd7a05c380a78d
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-05-07 16:29:06 +0900

    Title: 별 그리기, Time: 343ms, Memory: 14MB, Status: Passed - CodeTree

--- 240507/별 그리기/star-drawing.java
+++ 240507/별 그리기/star-drawing.java
@@ -2,25 +2,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int n = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = n; j > i; j--) {
+        // Step 1:
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= 2 * (i - 1) + 1; j++) {
-                System.out.print('*');
+            for(int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            for(int j = 0; j < n - i; j++) {
+                System.out.print(" ");
             }
             System.out.println();
         }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= i; j++) {
+        
+        // Step 2:
+        for(int i = n - 1; i >= 1; i--) {
+            for(int j = 0; j < n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = n; j >= 2 * (i - 1) + 1; j--) {
-                System.out.print('*');
+            for(int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            for(int j = 0; j < n - i; j++) {
+                System.out.print(" ");
             }
             System.out.println();
         }
