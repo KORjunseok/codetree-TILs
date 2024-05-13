6f7e560a2dcb94b6c379a478b0c32f9e8da735ff
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-05-13 19:44:36 +0900

    Title: 구구단 만들기, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240513/구구단 만들기/print-multiplication-table.java
+++ 240513/구구단 만들기/print-multiplication-table.java
@@ -14,6 +14,7 @@ public class Main {
                 if (j != 1)
                 System.out.print(" / ");
                 }
+                System.out.println();
             }
             else {
                 for (int j = 0; j < b / 2; j++) {
