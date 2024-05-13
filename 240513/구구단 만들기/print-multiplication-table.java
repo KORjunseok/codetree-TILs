4638f89d2c9a8703df3ebd904c6883d1c9bd2d32
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-05-13 20:12:15 +0900

    Title: 구구단 만들기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240513/구구단 만들기/print-multiplication-table.java
+++ 240513/구구단 만들기/print-multiplication-table.java
@@ -2,27 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        for (int i = 0; i < 9; i++) {
-            if (b - a == 0) {
-                for (int j = 0; j < 1; j++) {
-                    System.out.print(b - (a * j) + " * " + (i + 1) + " = " + (b - (a * j)) * (i + 1));
+        // b부터 a까지 감소하며 짝수의 구구단을 출력합니다.
+        for(int i = 1; i < 10; i++) {
+            for(int j = b; j >= a; j--) {
+                if(j % 2 == 0) {
+                    System.out.print(j + " * " + i + " = " + (i * j));
+                    if(j != a)
+                        System.out.print(" / ");
                 }
-                System.out.println();
-            }
-            else {
-                for (int j = 0; j < (b - a) - 1; j++) {
-                System.out.print(b - (2 * j) + " * " + (i + 1) + " = " + (b - (2 * j)) * (i + 1));
-                if (j != (b / 2) - 1)
-                System.out.print(" / ");
             }
             System.out.println();
-            }
-            
         }
     }
 }
