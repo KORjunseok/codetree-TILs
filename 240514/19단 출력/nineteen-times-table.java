1236435c0fc13164b8f4427f6d214cb9cf4912cf
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-05-14 17:55:45 +0900

    Title: 19단 출력, Time: 115ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240514/19단 출력/nineteen-times-table.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        for (int i = 1; i <= 19; i++) {
+            for (int j = 1; j <= 19; j++) {
+                System.out.print(i + " * " + j + " = " + i * j);
+                if (j != 19 && j % 2 == 1)
+                System.out.print(" / ");    
+                if (j % 2 == 0 || j == 19) 
+                System.out.println();
+            }
+            
+        }
+    }
+}
