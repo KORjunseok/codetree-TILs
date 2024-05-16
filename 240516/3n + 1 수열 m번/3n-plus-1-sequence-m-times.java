29ec4692fadf70f3734be532c1142a79fbf40d2e
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-05-16 11:42:39 +0900

    Title: 3n + 1 수열 m번, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240516/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int m = sc.nextInt();
+            for (int j = 1; j <= m; j++) {
+                while (m != 1) {
+                    if (m % 2 == 0) {
+                    m /= 2;
+                    cnt++;
+                    }
+                    else {
+                    m = (m * 3) + 1;
+                    cnt++;
+                    }
+                }
+                System.out.print(cnt);
+            }
+            System.out.println();
+        }
+    }
+}
