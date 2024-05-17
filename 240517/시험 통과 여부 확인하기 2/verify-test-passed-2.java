feb7074cbc275d374cab46518aaa8732056f155f
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-05-17 23:11:03 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240517/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[100];
+        // int[] sum = new int[n];
+        int sum = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr[j] = sc.nextInt();
+                // sum[i] += arr[j];
+                sum += arr[j];
+            }
+            if ((sum / 4) >= 60) {
+                System.out.println("pass");
+                cnt++;
+            } else 
+            System.out.println("fail");
+            sum = 0;
+        }
+            System.out.print(cnt);
+
+    }
+}
