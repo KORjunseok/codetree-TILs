b2f0aee33421ffee99a84fa1c1506a537a83602b
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-05-22 21:09:05 +0900

    Title: 점수대 파악하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240522/점수대 파악하기/find-out-the-score-range.java
+++ 240522/점수대 파악하기/find-out-the-score-range.java
@@ -5,7 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[100];
-        int[] cntArr = new int[100];
+        int[] cntArr = new int[11];
 
         for (int i = 0; i < 100; i++) {
             arr[i] = sc.nextInt();
