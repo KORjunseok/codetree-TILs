53b2b6ffc64d193fccb70237f061a9480290f322
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-05-23 22:58:24 +0900

    Title: 특정 위치의 문자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240523/특정 위치의 문자/char-in-specific-location.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        char[] arr = new char[]{'L', 'E', 'B', 'R', 'O', 'S'};
+        int cnt = -1;
+
+        for (int i = 0; i < 5; i++) {
+            char a = sc.next().charAt(0);
+            if (arr[i] == a) {
+                System.out.print(i);
+                cnt = 0;
+                break;
+            }
+        }
+        if (cnt == -1)
+        System.out.print("None");
+    }
+}
