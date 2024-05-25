360e46718569156922f05a7c00354ef7acd695d1
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-05-25 22:49:48 +0900

    Title: 500 근처의 수, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240525/500 근처의 수/near-500.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; 
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int minVal = 0;
+        int maxVal = 1000;
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        } 
+
+        for (int i = 1; i < 10; i++) {
+            if (arr[i] < 500) {
+                if (minVal < arr[i - 1]) {
+                    minVal = arr[i];
+                }
+            }
+            if (arr[i] > 500) {
+                if (maxVal > arr[i]) {
+                    maxVal = arr[i];
+                }
+            }
+        }
+        System.out.print(minVal + " " + maxVal);
+}
+}
