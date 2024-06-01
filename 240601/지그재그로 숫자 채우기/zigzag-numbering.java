a6eee7236924b1ef1f301ea1182966bbae3de620
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-06-01 22:33:47 +0900

    Title: 지그재그로 숫자 채우기, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240601/지그재그로 숫자 채우기/zigzag-numbering.java
+++ 240601/지그재그로 숫자 채우기/zigzag-numbering.java
@@ -6,14 +6,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
+        int cnt = 0;
 
         for (int i = 0; i < a; i++) {
             for (int j = 0; j < b; j++) {
                 if (j % 2 == 0)
-                System.out.print((i + j) + " ");
+                // System.out.print((i + j) + " ");
+                System.out.print(cnt + " ");
                 else
                 System.out.print((a * j * 2) - (i + 1) + " ");
             }
+            cnt++;
             System.out.println();
         }
 
