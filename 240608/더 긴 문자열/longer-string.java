3982bfe6efb05b32dff8c7e67f9db1189efe9c82
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-06-08 21:34:27 +0900

    Title: 더 긴 문자열, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240608/더 긴 문자열/longer-string.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        int ia = a.length();
+        int ib = a.length();
+
+        if (ia == ib)
+        System.out.print("same");
+        else if (ia > ib)
+        System.out.print(a + " " + ia);
+        else
+        System.out.print(b + " " + ib);
+
+    }
+}
