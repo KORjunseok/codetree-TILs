fd292842ae3befe2de2654d3355e843a8bc0f63c
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-06-12 21:48:21 +0900

    Title: 공백을 기준으로 출력, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240612/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+
+        String ab = a + b;
+        ab = ab.replace(" ", "");
+        System.out.print(ab);
+    }
+}
