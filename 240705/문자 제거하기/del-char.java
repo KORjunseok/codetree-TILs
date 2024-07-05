a721cbdb8cb49602ab0cfdd55bfb69ab1d2344bb
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-07-05 20:41:29 +0900

    Title: 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240705/문자 제거하기/del-char.java
+++ 240705/문자 제거하기/del-char.java
@@ -6,13 +6,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
 
-        for (int i = 0 ; i < 20; i++) {
+        for (int i = a.length(); i >= 0; i--) {
             int b = sc.nextInt();
             if (b > a.length()) {
-            System.out.println(a.charAt(0));
-            break;
+            a = a.substring(0, a.length() - 1);
             }
-            a = a.substring(0, b) + a.substring(b + 1);
+            else a = a.substring(0, b) + a.substring(b + 1);
+            
             System.out.println(a);
         }
 
