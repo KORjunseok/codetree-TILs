3b3e48ea1a4f31cde44d85ce4f67df7707fe52b5
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-07-14 19:52:17 +0900

    Title: 소문자와 숫자, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240714/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char[] arr = new char[100];
+        String b = "";
+
+        for (int i = 0; i < a.length(); i++) {
+            arr[i] = a.charAt(i);
+
+            if ((arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= '0' && arr[i] <= '9'))
+            b += arr[i];
+        }
+
+        b = b.toLowerCase();
+
+        System.out.print(b);
+        
+    }
+}
