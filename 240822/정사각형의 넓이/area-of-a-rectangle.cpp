2ae7a2c547c2b1eb768bbd993387d9aa49315b78
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-08-22 18:23:53 +0900

    Title: 정사각형의 넓이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240822/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    
+    cout << n * n << endl;
+    if (5 > n)
+    cout << "tiny";
+    return 0;
+}
