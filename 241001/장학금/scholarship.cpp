69b529b64e54f822a1fe7a2572abf4198ad627df
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-10-01 22:33:37 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241001/장학금/scholarship.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+
+    if (a >= 90) {
+        if (b >= 95 && 100 >= b) {
+            cout << 100000;
+        }
+        else if (b >= 90) {
+            cout << 50000;
+        }
+        else {
+            cout << 0;
+        }
+    }
+    else {
+        cout << 0;
+    }
+    return 0;
+}
