cd10323b30025ea4d366beb7efe46629251216f1
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-10-24 23:27:27 +0900

    Title: 증가시키며 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241024/증가시키며 출력하기/increase-and-print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    for (int i = 5; i <= 17; i += 2) {
+        cout << i << " ";
+    }
+    return 0;
+}
