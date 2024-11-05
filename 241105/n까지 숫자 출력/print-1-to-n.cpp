dd2991ab0b9c6b95b5c06104299048cabd43caa0
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-11-05 23:07:07 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241105/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int a = 1;
+    while (a <= n) {
+        cout << a << " ";
+        a++;
+    }
+    return 0;
+}
