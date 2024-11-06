ed921705bc39cd72a476dd40af5e2e31d3949ed9
Oh Junseok <131343782+KORjunseok@users.noreply.github.com>
2024-11-06 21:56:19 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241106/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, a = 1;
+    cin >> n;
+    while (a <= n) {
+        cout << "*" << endl;
+        a++;
+    }
+    return 0;
+}
